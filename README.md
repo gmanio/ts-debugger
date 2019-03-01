64df9f770c2f655bcacf9a55376dfe0cd8574293
jimanPark <im@gman.io>
2019-03-01 19:53:38 +0900

    [feature] added readme

---
+++ README.md
@@ -0,0 +1,1 @@
+typescript debugging in vscode
--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { Employee } from "./entity/Employee";
-``
+
 createConnection().then(async connection => {
 
   let employees = await connection.getRepository(Employee);
